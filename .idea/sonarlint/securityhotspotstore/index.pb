2b55ff019b1a361d5f2072f89506ce3b55aa511b
SergioWindows <sandres0190@gmail.com>
2023-10-03 10:55:25 -0500

    feat(App): Pruebas sobre Widget
    Agregamos una prueba de como adicionar Widgets de pantalla Home
    
    usando el método antiguo, a hoy en dia existe otra opción en beta.

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+r
+Bapp/src/test/java/com/sergio/rodriguez/artspace/ExampleUnitTest.kt,c\b\cbae5a0256f93a264ac89bd515b76fde001be5ad
+Å
+Qapp/src/androidTest/java/com/sergio/rodriguez/artspace/ExampleInstrumentedTest.kt,2\f\2f09ecee9a632a47d47b3f517c10c890b0924dbd
+o
+?app/src/main/java/com/sergio/rodriguez/artspace/MainActivity.kt,9\b\9be88e3ffe8b9ce294d2630a4c1ffb43d42d9aeb
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
+r
+Bapp/src/main/java/com/sergio/rodriguez/artspace/PruebaAppWidget.kt,c\f\cfd92435779e79139f6cd55d14e7a66027f27a5f
+q
+Aapp/src/main/java/com/sergio/rodriguez/artspace/ui/theme/Color.kt,6\5\65cec954c0e08378cace4337d989e3b27ead6624
+q
+Aapp/src/main/java/com/sergio/rodriguez/artspace/ui/theme/Theme.kt,b\a\ba1461e5a11bb2aaa34221bc781599b6af48166a
--- app/src/main/java/com/sergio/rodriguez/artspace/ui/theme/Theme.kt
+++ app/src/main/java/com/sergio/rodriguez/artspace/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.sergio.rodriguez.artspace.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -40,11 +41,10 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun ArtSpaceTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = true, // Dynamic color is available on Android 12+
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    val colorScheme: ColorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
